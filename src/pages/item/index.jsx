fda4851e7b701bcc57d223cf11e1e6f82f182f8d
fenglingcong <1539027251@qq.com>
2019-08-26 16:04:42 +0800

    'detailgarrery'

--- src/pages/item/index.jsx
+++ src/pages/item/index.jsx
@@ -3,6 +3,7 @@ import { View } from '@tarojs/components'
 import './index.scss'
 import { connect } from '@tarojs/redux';
 import * as actions from '@actions/item'
+import Gallery from './gallery'
 
 @connect(state => state.item, { ...actions })
 export default class Item extends Component {
@@ -32,9 +33,19 @@ export default class Item extends Component {
 
   render () {
     const { itemInfo } = this.props
+    const { itemDetail = {} } = itemInfo
+    const gallery = [
+      itemInfo.listPicUrl,
+      itemDetail.picUrl1,
+      itemDetail.picUrl2,
+      itemDetail.picUrl3,
+      itemDetail.picUrl4,
+    ]
+    console.log(this.props)
     return (
       <View>
         我是详情页
+        {gallery.length > 0 && <Gallery list={gallery} />}
         {JSON.stringify(itemInfo)}
       </View>
     )
