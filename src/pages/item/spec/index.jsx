8e0fa7c14012a64b149e2980322ac1704510aacf
fenglingcong <1539027251@qq.com>
2019-08-28 21:56:40 +0800

    'update'

--- src/pages/item/spec/index.jsx
+++ src/pages/item/spec/index.jsx
@@ -1,15 +1,13 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
-// import { Loading } from '@components/index'
+import { Counter } from '@components'
 import './index.scss'
-
-// console.log(Loading)
 
 export default class Spec extends Component {
   render () {
     return (
       <View>
-        {/* <Loading /> */}
+        <Counter />
         我是spec
       </View>
     )
