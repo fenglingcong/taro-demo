8c0071a097782b0c10e4a8cf39f866e029edf130
fenglingcong <1539027251@qq.com>
2019-08-29 11:38:57 +0800

    counter

--- src/components/counter/index.jsx
+++ src/components/counter/index.jsx
@@ -1,12 +1,60 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
+import { View, Image, Text } from '@tarojs/components'
+import classNames from 'classnames'
+import minusIcon from './assets/minus.png'
+import minusDisabledIcon from './assets/minus-disabled.png'
+import plusIcon from './assets/plus.png'
 import './index.scss'
 
 export default class Counter extends Component {
+  static defaultProps = {
+    num: 0,
+    compStyle: '',
+    optStyle: '',
+    numStyle: '',
+    onChange: () => {}
+  }
+
+  handleMinus = () => {
+    if (this.props.num > 1) {
+      this.props.onChange(this.props.num - 1)
+    }
+  }
+
+  handlePlus = () => {
+    this.props.onChange(this.props.num + 1)
+  }
+
   render () {
+    const { num, compStyle, numStyle } = this.props
+    const isMinusDidabled = num <= 1
+
     return (
-      <View>
-        我是counter
+      <View className='counter' style={compStyle}>
+        <View
+          className={classNames('counter-minus', isMinusDidabled && 'counter-minus--disabled')}
+          onClick={this.handleMinus}
+        >
+          <Image
+            className='counter-minus__img'
+            src={isMinusDidabled ? minusDisabledIcon : minusIcon}
+          />
+        </View>
+        <View
+          className='counter-num'
+          style={numStyle}
+        >
+          <Text className='counter-num__txt'>{num}</Text>
+        </View>
+        <View
+          className='counter-plus'
+          onClick={this.handlePlus}
+        >
+          <Image
+            className='counter-plus__img'
+            src={plusIcon}
+          />
+        </View>
       </View>
     )
   }
--- src/pages/item/spec/index.jsx
+++ src/pages/item/spec/index.jsx
@@ -4,10 +4,21 @@ import { Counter } from '@components'
 import './index.scss'
 
 export default class Spec extends Component {
+  state = {
+    cnt: 1
+  }
+
+  handleUpdate = (cnt) => {
+    this.setState({ cnt })
+  }
+
   render () {
     return (
       <View>
-        <Counter />
+        <Counter
+          num={this.state.cnt}
+          onChange={this.handleUpdate}
+        />
         我是spec
       </View>
     )
